12c4739c0de45327a46958b1394121f08c02d6d8
Phonglt1210 <3dcheckin@gmail.com>
2025-07-16 00:53:30 +0700

    Create tool_obfuscated.js

---
+++ tool_obfuscated.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name         Fonsida Obfuscated Tool (Demo)
+// ==/UserScript==
+
+console.log("🛡️ Fonsida Tool đã được tải từ server thành công!");
+alert("✅ Fonsida Tool đã hoạt động!");
