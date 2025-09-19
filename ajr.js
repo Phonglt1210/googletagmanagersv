e18dbc4100dc14b996dc6327129af67601675ecb
Phonglt1210 <3dcheckin@gmail.com>
2025-09-19 14:31:58 +0700

    Create ajr.js

---
+++ ajr.js
@@ -0,0 +1,54 @@
+// ==UserScript==
+// @name         Auto Click Outside Game Room (Zigavn)
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  Tự động click khi đang ở phòng chờ, không click nếu đang trong bàn chơi
+// @author       FonArt
+// @match        *://zigavn.com/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    let inGame = false;
+
+    // Hook logMessage để phát hiện vào/thoát bàn
+    const originalLogMessage = window.logMessage;
+    window.logMessage = function (msg) {
+        if (msg.includes("ProcessGameTableInfo")) {
+            console.log("[AutoClick] Đã vào bàn chơi");
+            inGame = true;
+        }
+        if (msg.includes("ProcessOutGame")) {
+            console.log("[AutoClick] Đã thoát bàn chơi");
+            inGame = false;
+        }
+        return originalLogMessage.apply(this, arguments);
+    };
+
+    // Hàm tự động click khi không ở trong bàn
+    function autoClickWhenIdle() {
+        if (!inGame) {
+            console.log("[AutoClick] Đang ở sảnh, click tìm bàn...");
+            clickAt(250, 550);  // Thay đổi toạ độ nếu cần
+        } else {
+            console.log("[AutoClick] Đang trong bàn, không click");
+        }
+    }
+
+    // Hàm click tại toạ độ cụ thể
+    function clickAt(x, y) {
+        const target = document.elementFromPoint(x, y);
+        if (target) {
+            const down = new MouseEvent('mousedown', { bubbles: true, clientX: x, clientY: y });
+            const up = new MouseEvent('mouseup', { bubbles: true, clientX: x, clientY: y });
+            target.dispatchEvent(down);
+            target.dispatchEvent(up);
+        }
+    }
+
+    // Bắt đầu chạy mỗi 3 giây
+    setInterval(autoClickWhenIdle, 50);
+
+})();
