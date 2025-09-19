578642d2f7ae0c787e669a70b6ac77de6288213a
Phonglt1210 <3dcheckin@gmail.com>
2025-09-19 14:19:04 +0700

    Update jr.js

--- jr.js
+++ jr.js
@@ -4,7 +4,7 @@
 // @version      1.0
 // @description  J R
 // @author       FonArt
-// @match        *://zigavn.com/*
+// @match        https://zigavn.com/*
 // @grant        none
 // ==/UserScript==
 
